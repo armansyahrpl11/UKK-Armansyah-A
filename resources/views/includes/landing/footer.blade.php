74ef62b2e809283be0186a3db1dcfe87123f812e
alawi11 <muhamadalawi981@gmail.com>
2023-03-17 18:38:34 +0700

    first commit

---
+++ resources/views/includes/landing/footer.blade.php
@@ -0,0 +1,7 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+        <div class="text-center">
+            ©CIPON 2023 <strong></strong>
+        </div>
+  </footer><!-- End Footer -->
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>CIPON</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.svg" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+  @include('includes.landing.stylesheet')
+
+
+  <!-- =======================================================
+    * Template Name: Vesperr - v4.7.0
+    * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+  </head>
+
+  <body>
+
+    @include('includes.landing.navbar')
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">Layanan Masyarakat Desa Ciherang Pondok</h1>
+          <h2 data-aos="fade-up" data-aos-delay="400">Sampaikan laporan masalah Anda di sini, kami akan memprosesnya dengan cepat , aman dan nyaman.</h2>
+          <div data-aos="fade-up" data-aos-delay="800">
+            <a href="{{ url('login')}}" class="btn-get-started scrollto">Laporkan!</a>
+          </div>
+        </div>
+        {{-- <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+          <img src="assets/img/wikrama-logo.png" class="img-fluid animated" alt="">
+        </div> --}}
+      </div>
+      
+    </div>
+    
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+
+    @include('includes.landing.footer')
+
+
+
+
+
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  @include('includes.landing.javascript')
+
+</body>
+
+</html>
